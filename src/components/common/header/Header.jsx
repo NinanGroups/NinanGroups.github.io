7b25ee9cb24a5ac2225812eb7b2524f84ee1d8ca
Meenatchi Kumaran <meena.programmer@gmail.com>
2023-12-17 14:04:42 +0530

    new repo

---
+++ src/components/common/header/Header.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from "react"
+import { Link } from "react-router-dom"
+import Head from "./Head"
+import "./header.css"
+
+const Header = () => {
+  const [click, setClick] = useState(false)
+
+  return (
+    <>
+      <Head />     
+    </>
+  )
+}
+
+export default Header
